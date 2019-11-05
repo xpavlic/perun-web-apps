9678664a524a734b46f9127d4c6c306e31ea1d3a
xkureck <445264@mail.muni.cz>
2019-11-05 14:15:33 +0100

    App configuration * add runtime configuration * finish footer(data are now taken from config file)

---
+++ apps/admin-gui/src/app/core/services/common/app-config.service.ts
@@ -0,0 +1,63 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AuthService } from './auth.service';
+import { environment } from '../../../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppConfigService {
+
+  private instanceConfig;
+  private defaultConfig;
+
+  constructor(private http: HttpClient,
+              private authService: AuthService) { }
+
+  /**
+   * Load default configuration.
+   * If instance is not in production mode configuration it is also
+   * taken as instance configuration and start authentication.
+   */
+  loadAppDefaultConfig() : Promise<void> {
+    return this.http.get('/assets/config/defaultConfig.json')
+      .toPromise()
+      .then(config => {
+        this.defaultConfig = config;
+        if (!environment.production) {
+          this.instanceConfig = config;
+          this.authService.loadConfigData(this.instanceConfig);
+        }
+      });
+  }
+
+  /**
+   * Load instance configuration.
+   * If instance is in production mode configuration mode is assigned to
+   * instance config and start authentication.
+   */
+  loadAppInstanceConfig() : Promise<void>  {
+    return this.http.get('/assets/config/instanceConfig.json')
+      .toPromise()
+      .then(data => {
+        if (environment.production) {
+          this.instanceConfig = data;
+          this.authService.loadConfigData(this.instanceConfig);
+        }
+      });
+  }
+
+  /**
+   * Get key from json configuration. If key is not present in instance
+   * configuration method returns value from default configuration.
+   * @param key
+   */
+  get(key: any) {
+    const value = this.instanceConfig[key];
+    if (value) {
+      return value;
+    } else {
+      return this.defaultConfig[key];
+    }
+  }
+}
--- apps/admin-gui/src/environments/environment.cesnet-dev.ts
+++ apps/admin-gui/src/environments/environment.cesnet-dev.ts
@@ -1,10 +1,3 @@
 export const environment = {
   production: true,
-  api_url: 'https://perun-dev.cesnet.cz/oauth/rpc/',
-  oauth_authority: 'https://login.cesnet.cz/oidc/',
-  oauth_callback: 'https://gui-dev.perun-aai.org/api-callback',
-  oauth_client_id: '411471d9-5e3d-48bd-9717-25f1fa634d20',
-  oauth_redirect_uri: 'https://gui-dev.perun-aai.org/api-callback',
-  oauth_post_logout_redirect_uri: 'https://gui-dev.perun-aai.org/',
-  oauth_silent_redirect_uri: 'https://gui-dev.perun-aai.org/silent-refresh.html'
 };
