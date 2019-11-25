2c432a3e3b9398444fc324a4084dcc81cac98a2f
Martin Kostka <mkostka62@gmail.com>
2019-11-25 13:47:25 +0100

    Scroll to top of a page after routing
    * After routing, page is automatically scrolled to the top

--- apps/admin-gui/src/app/app-routing.module.ts
+++ apps/admin-gui/src/app/app-routing.module.ts
@@ -80,7 +80,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
-    preloadingStrategy: PreloadAllModules
+    preloadingStrategy: PreloadAllModules,
+    scrollPositionRestoration: 'enabled'
   })],
   exports: [RouterModule]
 })
