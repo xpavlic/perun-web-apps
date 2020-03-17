3c8351d8baf75e3f30a89745f37812656f3678c7
Vojtěch Sassmann <vojtech.sassmann@gmail.com>
2020-03-17 09:39:49 +0100

    Fixed imports (#176)

--- libs/perun/services/src/index.ts
+++ libs/perun/services/src/index.ts
@@ -1,8 +1,6 @@
 export { MembersService } from './lib/members.service';
 export { PerunApiService } from './lib/perun-api-service';
 export { RtMessagesService } from './lib/rt-messages.service';
-export { ServiceService } from './lib/service.service';
-export { ExtSourceService } from './lib/ext-source.service';
 export { AuthResolverService } from './lib/auth-resolver.service';
 export { AuthService } from './lib/auth.service';
 export { StoreService } from './lib/store.service';
