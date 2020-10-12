add80c7573d762cf01acf78dc6c71280b3c0a83d
Martin Kostka <mkostka62@gmail.com>
2020-10-12 14:40:25 +0200

    User profile - External services
    * Added config option to add link to external services

--- apps/user-profile/src/app/services/side-menu-item.service.ts
+++ apps/user-profile/src/app/services/side-menu-item.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
+import { StoreService } from '@perun-web-apps/perun/services';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SideMenuItemService {
 
-  constructor() { }
+  constructor(private store: StoreService) { }
 
   getSideMenuItems(): SideMenuItem[] {
-    return [
+    const items: SideMenuItem[] = [
       {
         label: 'MENU_ITEMS.PROFILE',
         icon: 'account_box',
@@ -58,79 +59,21 @@ export class SideMenuItemService {
         activatedRegex: '^/profile/settings',
         tabName: 'settings'
       }
-    ]
+    ];
+    const externalServices = this.store.get('external_services');
+    externalServices.forEach(service => {
+      items.push( {
+        label: service['label_en'],
+        label_cz: service['label_cz'],
+        icon: 'insert_link',
+        link: service['url'],
+        activatedRegex: '^/profile/external',
+        tabName: 'external',
+        external: true
+      })
+    })
+    return items;
   }
-
-  // getProfileItem(): SideMenuItem {
-  //   return {
-  //     label: 'MENU_ITEMS.PROFILE',
-  //     icon: 'account_box',
-  //     link: '/profile',
-  //     activatedRegex: '^/profile$',
-  //     tabName: 'profile'
-  //   };
-  // }
-
-  // getIdentitiesItem(): SideMenuItem {
-  //   return {
-  //     label: 'MENU_ITEMS.IDENTITIES',
-  //     icon: 'remove_red_eye',
-  //     link: '/profile/identities',
-  //     activatedRegex: '^/profile/identities$',
-  //     tabName: 'identities'
-  //   };
-  // }
-
-  // getServicesItem(): SideMenuItem {
-  //   return {
-  //     label: 'MENU_ITEMS.SERVICES',
-  //     icon: 'build',
-  //     link: '/profile/services',
-  //     activatedRegex: '^/profile/services$',
-  //     tabName: 'services'
-  //   };
-  // }
-
-  // getGroupsItem(): SideMenuItem {
-  //   return {
-  //     label: 'MENU_ITEMS.GROUPS',
-  //     icon: 'group',
-  //     link: '/profile/groups',
-  //     activatedRegex: '^/profile/groups$',
-  //     tabName: 'groups'
-  //   };
-  // }
-
-  // getVosItem(): SideMenuItem {
-  //   return {
-  //     label: 'MENU_ITEMS.VOS',
-  //     icon: 'account_balance',
-  //     link: '/profile/organizations',
-  //     activatedRegex: '^/profile/organizations$',
-  //     tabName: 'vos'
-  //
-  //   };
-  // }
-
-  // getPrivacyItem(): SideMenuItem {
-  //   return {
-  //     label: 'MENU_ITEMS.PRIVACY',
-  //     icon: 'vpn_key',
-  //     link: '/profile/privacy',
-  //     activatedRegex: '^/profile/privacy$',
-  //     tabName: 'privacy'
-  //   };
-  // }
-
-  // getSettingsItem(): SideMenuItem {
-  //   return {
-  //     label: 'MENU_ITEMS.SETTINGS',
-  //     icon: 'settings',
-  //     link: '/profile/settings',
-  //     activatedRegex: '^/profile/settings',
-  //     tabName: 'settings'
-  //   };
-  // }
 }
 
 export interface SideMenuItem {
@@ -139,4 +82,6 @@ export interface SideMenuItem {
   link: string;
   activatedRegex: string;
   tabName: string;
+  external?: boolean;
+  label_cz?: string;
 }
