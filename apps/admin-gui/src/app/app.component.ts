f53ac1fc0335e4cf00280cc9b95843268fc305ad
Vojtěch Sassmann <445320@mail.muni.cz>
2020-10-30 11:04:46 +0100

    Configurable warning bar
    * The top bar is now hidden by default, and can be displayed by setting
    the `display_warning` to true. Also, the message can be configured to
    something else by setting the `warning_message` option.

--- apps/admin-gui/src/app/app.component.ts
+++ apps/admin-gui/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component, HostListener, OnInit } from '@angular/core';
 import { CacheHelperService } from './core/services/common/cache-helper.service';
 import { StoreService } from '@perun-web-apps/perun/services';
 import { PerunPrincipal } from '@perun-web-apps/perun/openapi';
-import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -26,11 +25,13 @@ export class AppComponent implements OnInit {
   lastScreenWidth: number;
 
   principal: PerunPrincipal;
-  isProduction = false;
   navBackgroundColor = this.store.get('theme', 'nav_bg_color');
   sideBarBorderColor = this.store.get('theme', 'sidemenu_border_color');
   contentBackgroundColor = this.store.get('theme', 'content_bg_color');
   sideMenubackgroundColor = this.store.get('theme', 'sidemenu_bg_color');
+
+  displayWarning: boolean = this.store.get('display_warning');
+  warningMessage: string = this.store.get('warning_message');
 
   @HostListener('window:resize', ['$event'])
   getScreenSize(event?) {
@@ -44,24 +45,23 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.isProduction = environment.production;
     this.principal = this.store.getPerunPrincipal();
   }
 
   getTopGap() {
-    return environment.production ? 112 : 64;
+    return this.displayWarning ? 112 : 64;
   }
 
   getSideNavMarginTop() {
-    return environment.production ? '112px' : '64px';
+    return this.displayWarning ? '112px' : '64px';
   }
 
   getSideNavMinHeight() {
-    return environment.production ? 'calc(100vh - 112px)' : 'calc(100vh - 64px)';
+    return this.displayWarning ? 'calc(100vh - 112px)' : 'calc(100vh - 64px)';
   }
 
   getNavMenuTop() {
-    return environment.production ? '48px' : '0';
+    return this.displayWarning ? '48px' : '0';
   }
 
   getContentInnerMinHeight() {
@@ -70,6 +70,6 @@ export class AppComponent implements OnInit {
 
     let footerSpace = this.isMobile() ? '510' : '210';
     footerSpace = '0';
-    return environment.production ? 'calc((100vh - 112px) + ' + footerSpace + 'px)' : 'calc((100vh - 64px) + ' + footerSpace + 'px)';
+    return this.displayWarning ? 'calc((100vh - 112px) + ' + footerSpace + 'px)' : 'calc((100vh - 64px) + ' + footerSpace + 'px)';
   }
 }
